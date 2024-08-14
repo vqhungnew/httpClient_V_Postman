a8240044f4c1d2aa50d318dd04aca33ba22b6199
Vu Quoc Hung <vqhungnew@gmail.com>
2024-08-14 15:35:29 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿var client = new HttpClient();
+var request = new HttpRequestMessage(HttpMethod.Get, "https://ipinfo.io/115.73.211.45/geo");
+var response = await client.SendAsync(request);
+response.EnsureSuccessStatusCode();
+Console.WriteLine(await response.Content.ReadAsStringAsync());
